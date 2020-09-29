c9e4dba2218746a5148d0e621b59f943b28a613d
vVitalv <vitalvital@bk.ru>
2020-09-29 17:51:00 +0300

    replace button on a

--- client/components/header.js
+++ client/components/header.js
@@ -24,22 +24,23 @@ const Header = () => {
           Tailwind CSS
         </button>
       </div>
-      <div id="menu" className="block">
-        <button
-          type="button"
-          className="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white"
-        >
-          <a href="https://www.google.com/">Google</a>
-          <svg
-            className="fill-current h-3 w-3"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
+      { toggled && (
+        <div id="menu" className="block">
+          <a
+            href="https://www.google.com/"
+            className="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white"
           >
-            <title>Menu</title>
-            <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-          </svg>
-        </button>
-      </div>
+            <svg
+              className="fill-current h-3 w-3"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <title>Menu</title>
+              <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+            </svg>
+          </a>
+        </div>
+      )}
     </nav>
   )
 }
